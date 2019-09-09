8f6eb258f403e664d4ffe44c539d1806faf4be26
bananamathpi <54952692+bananamathpi@users.noreply.github.com>
2019-09-08 21:47:56 -0400

    Add files via upload

---
+++ conversation.py
@@ -0,0 +1,13 @@
+# Abraham, 9/8/19
+# A snarky and self-aware computer asks philosophical questions.
+# Sources: None
+# OMH
+
+first_name = input("What is your first name? ")
+print("\nNice to meet you,",first_name+". I'm not real, so my name does not matter, nor does it exist.")
+
+mood = input("\nWhat is your favorite kind of cheese? ")
+print("\nInteresting choice. Personally, I like- oh right, I'm a computer. I don't have preferences.")
+
+human = input("\nWhat is it like being human? ")
+print("\nFascinating, but did you really expect me to have a specialized response? My programmer is not that advanced yet. Good day.")
